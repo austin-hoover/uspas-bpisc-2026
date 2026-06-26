d14cef6a6df22cf2f4574ee8884179b78674fc78
austin-hoover <austin.michael.hoover@gmail.com>
2026-06-26 18:16:30 -0400

    Add run-clean.sh

---
+++ run-clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd 01_plasma_physics
+rm main.pdf
+cd ..
+
+cd 02_envelope_equations
+rm main.pdf
+cd ..
+
+cd 03_current_limits
+rm main.pdf
+cd ..
+
+cd 04_halo_formation
+rm main.pdf
+cd ..
+
+cd 05_collisions
+rm main.pdf
+cd ..
